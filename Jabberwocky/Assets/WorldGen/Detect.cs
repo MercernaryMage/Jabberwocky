a5d30c9d036d8711d26f139e07a7d6bc1ba56c90
Perry <perryt@wz.hasbro.com>
2021-11-18 16:38:38 -0800

    Insainity

---
+++ Jabberwocky/Assets/WorldGen/Detect.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Detect : MonoBehaviour
+{
+    public bool dead = false;
+
+    private void Start()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 3);
+
+        foreach (Collider c in colliders)
+        {
+            if (c.tag == "Floor")
+            {
+                continue;
+            }
+            if (c.tag == "Player")
+            {
+                continue;
+            }
+            if (c.gameObject == gameObject)
+            {
+                continue;
+            }
+            Detect d = c.gameObject.GetComponent<Detect>();
+            if (d && d.dead == true)
+            {
+                continue;
+            }
+            Debug.Log(c.gameObject.name);
+            dead = true;
+            Destroy(gameObject);
+            break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//test
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
